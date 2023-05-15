fc374c719761098eaff3e97b48f9dd8084a9937b
Ben Gabay <ben.gabay38@gmail.com>
2023-05-15 13:25:20 +0300

    fix vercel analytics

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './components/App/App';
+import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
+        <Analytics />;
         <App />
     </React.StrictMode>
 );
